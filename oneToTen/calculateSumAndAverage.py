9866e8bc63a4383e59b08072206aea69a132d4af
Shinnamon-Roll <chimonkung0@gmail.com>
2024-08-09 01:53:08 +0700

    build calculate program no release

---
+++ oneToTen/calculateSumAndAverage.py
@@ -0,0 +1,21 @@
+def calculateSumAndAverage(number):
+    
+    temp
+    for num in number:
+
+    print(number)
+    
+def main():
+
+    numSets = []
+    count = 1
+
+    while True:
+        number = float(input("Enter the number :" ))
+
+        numSets.append(number)
+        count += 1
+
+        calculateSumAndAverage(numSets)
+
+main()
