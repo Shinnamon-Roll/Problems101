e704565230dd87c8361aefd2e1b19edefbc7b640
Shinnamon-Roll <chimonkung0@gmail.com>
2024-08-11 22:13:59 +0700

    build checkprime program

---
+++ oneToTen/primeNumberChecker.py
@@ -0,0 +1,26 @@
+def checkPrime(numbers):
+    divided = []
+
+    for num in range(1, numbers + 1):
+        if numbers % num == 0:
+            divided.append(num)
+    
+    if divided[0] == 1 and divided[1] == numbers :
+        print("Prime numbers")
+
+    else:
+        print("not prime numbers")
+
+def main():
+    while True:
+        number = int(input("\nEnter a number : "))
+        if number > 1 and number <= 10000:
+            break
+        elif number == 1:
+            print("It's not prime...Please try again.")
+            pass
+        else:
+            print("Input number incorrect...Please try again.") 
+            pass
+    checkPrime(number)
+main()
