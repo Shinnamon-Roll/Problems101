ef711c0c02847fb228eb05d7cb723d53a4380c44
Shinnamon-Roll <chimonkung0@gmail.com>
2024-09-04 00:40:59 +0700

    fix word search in list

--- 11-20/17WordSearchInList.py
+++ 11-20/17WordSearchInList.py
@@ -6,10 +6,13 @@ def isWordInList(word_list: list[str], search_term: str) -> bool:
             return False
 
 def main():
-    word_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
-    searchWord = input(f"{word_list}\nwhat word do you want to search : ")
+    wordList = []
+    for i in range(10):
+        inputword = input(f"{wordList}\nenter your word to add in list : ")
+        wordList.append(inputword)
 
-    results = isWordInList(word_list, searchWord)
+    searchWord = input(f"{wordList}\nwhat word do you want to search : ")
+    results = isWordInList(wordList, searchWord)
     print(results)
 
 main()
