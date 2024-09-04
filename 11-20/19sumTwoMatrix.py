e937899781172eeca97084dc441dc1960b562c32
Shinnamon-Roll <chimonkung0@gmail.com>
2024-09-04 12:17:58 +0700

    update try to do 19

--- 11-20/19sumTwoMatrix.py
+++ 11-20/19sumTwoMatrix.py
@@ -2,6 +2,8 @@ def sum_matrices(matrix1: List[List[int]], matrix2: List[List[int]]) -> List[Lis
     print("heeyai")
 
 def main():
-    print("hee kao")
+    matrix1 = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+    matrix2 = [[4,3,2,1],[4,3,2,1],[4,3,2,1]]
+    
 
 main()
