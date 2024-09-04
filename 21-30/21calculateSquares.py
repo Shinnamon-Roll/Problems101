4e12fa134c18a73b4f3f48adc854351740ccebe5
Shinnamon-Roll <chimonkung0@gmail.com>
2024-09-04 16:15:31 +0700

    update

--- 21-30/21calculateSquares.py
+++ 21-30/21calculateSquares.py
@@ -1,12 +1,24 @@
 def calculateStatistics(t: tuple[int, ...]) -> tuple[tuple[int, ...], int, int, int, float]:
-    squaredValues = ()
+    squaredValueslist = []
+    Summernumber = 0
     for i in t:
-        cal = i * t
-        print(cal)
+        cal = i ** 2
+        squaredValueslist.append(cal)
+
+    maxValue = max(squaredValueslist)
+    minValue = min(squaredValueslist)
+    for j in squaredValueslist:
+        Summernumber += j 
+
+    averag_num = Summernumber/len(t)
+    squaredValues = tuple(squaredValueslist)
+
+    print(f"\nsquared values is : {squaredValues}\nmax value is : {maxValue}\nmin value is : {minValue}\nAverage value is : {averag_num}")
+        
 
 def main():
     number = (1,2,3,4,5,6,7,8,9,10)
-    results = calculateStatistics(number)
-    print(results)
+    calculateStatistics(number)
+    # print(results)
 
 main()
---
+++ 21-30/22CreateaDictionary.py
@@ -0,0 +1,8 @@
+def createadictionary(list1:list[int],list2:list[str]) -> Dict[int, str]:   
+    Dict = {}
+    return Dict
+def main():
+    list1 = [1, 2, 3, 4]
+    list2 = ["blue", "green", "pink", "yellow"]
+    createadictionary(list1, list2)
+    pass
