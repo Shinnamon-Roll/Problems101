709323d87631b58888b13614beb97b55b3060bec
Shinnamon-Roll <chimonkung0@gmail.com>
2024-09-05 14:04:40 +0700

    update finish dictionary

--- 21-30/22CreateaDictionary.py
+++ 21-30/22CreateaDictionary.py
@@ -1,8 +1,31 @@
-def createadictionary(list1:list[int],list2:list[str]) -> Dict[int, str]:   
-    Dict = {}
-    return Dict
+def createDictionary(list1:list[int],list2:list[str]) -> dict[int, str]:   
+    dict = {}
+    value = 0
+    
+    for key in list1:
+        if list2[value] == "done":
+            break
+        dict[key] = list2[value]
+        value += 1
+        
+    print(dict)
+
 def main():
-    list1 = [1, 2, 3, 4]
-    list2 = ["blue", "green", "pink", "yellow"]
-    createadictionary(list1, list2)
-    pass
+    list1 = []
+    list2 = []
+    number = 1
+
+    while True:
+        word = input("Enter a word for list (done for stop) : ")
+        list1.append(number)
+        list2.append(word)
+        number += 1
+
+        if word == "done":
+            break 
+        if len(list1) > 100:
+            break
+
+    createDictionary(list1, list2)
+    
+main()
