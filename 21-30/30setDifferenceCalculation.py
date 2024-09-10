61853dda5fdddbce7101ea84358122081bb47780
Shinnamon-Roll <chimonkung0@gmail.com>
2024-09-11 01:16:31 +0700

    update

---
+++ 21-30/30setDifferenceCalculation.py
@@ -0,0 +1,11 @@
+def calculateSetDifferences(set1: set, set2: set) -> tuple[set, set]:
+    
+
+def main():
+    set1 = {'a', 'b', 'c'}
+    set2 = {'b', 'c', 'd'}
+    
+    result = calculateSetDifferences(set1, set2)
+    print(result)
+
+main()
