76ceb10d292858253d45409f5bca6b6faa2d52ae
Shinnamon-Roll <chimonkung0@gmail.com>
2024-09-14 12:43:33 +0700

    update country Sale Analysis

--- 31-40/countrySaleAnalysis.py
+++ 31-40/countrySaleAnalysis.py
@@ -1,7 +1,22 @@
-def highest_sales_country(sales: dict[str, int]) -> tuple[str, int]:
-    print("heeyai")
+def highestSalesCountry(sales: dict[str, int]) -> tuple[str, int]:
+    temp = {}
+    check = 0
+    for key, values in sales.items():
+        if values > check:
+            temp = {}
+            temp[key] = values
+            check = values
+
+    return temp
 
 def main():
-    
+    salesData = {}
+    for _ in range(5):
+        inputKey, inputValue = input("Enter your Country and sales : ").split()
+        salesData[inputKey] = int(inputValue)
+        print(salesData)
+
+    result = highestSalesCountry(salesData)
+    print(result)
 
 main()
