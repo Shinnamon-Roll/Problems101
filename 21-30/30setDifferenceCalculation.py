d2f4f545934be2995f1ae4a9bee68c54e95a2494
Shinnamon-Roll <chimonkung0@gmail.com>
2024-09-14 11:57:15 +0700

    update

--- 21-30/30setDifferenceCalculation.py
+++ 21-30/30setDifferenceCalculation.py
@@ -1,5 +1,7 @@
 def calculateSetDifferences(set1: set, set2: set) -> tuple[set, set]:
-    
+    result1 = set1.difference(set2)
+    result2 = set2.difference(set1)
+    return result1, result2
 
 def main():
     set1 = {'a', 'b', 'c'}
---
+++ 31-40/countrySaleAnalysis.py
@@ -0,0 +1,7 @@
+def highest_sales_country(sales: dict[str, int]) -> tuple[str, int]:
+    print("heeyai")
+
+def main():
+    
+
+main()
