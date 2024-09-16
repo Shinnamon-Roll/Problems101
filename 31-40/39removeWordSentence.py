0a426d192cbb1e01692531851aa18b5315d01845
Shinnamon-Roll <chimonkung0@gmail.com>
2024-09-16 13:38:58 +0700

    feat remove word sentence

--- 31-40/39removeWordSentence.py
+++ 31-40/39removeWordSentence.py
@@ -1,7 +1,22 @@
-def remove_word(sentence: str, wordToRemove: str) -> str:
-    print("heeyai")
+def removeWord(sentence: str, wordToRemove: str) -> str:
+    if wordToRemove in sentence:
+        newSentence = ' '.join(sentence.split()).replace(wordToRemove, '').replace("  ", " ").strip()
+        print(f"Updated sentence: {newSentence}")
+        return newSentence
+    else:
+        print("Warning: wordToRemove is not in the sentence")
+        return sentence
 
 def main():
-    print("hee kao")
+    data = input("Enter a sentence: ")
+    check = True
+    while check:
+        wordToRemove = input("Remove word: ")
+        if wordToRemove.lower() == 'exit':
+            print(data)
+            print("Exiting...")
+            check = False
+        else:
+            data = removeWord(data, wordToRemove)
 
 main()
