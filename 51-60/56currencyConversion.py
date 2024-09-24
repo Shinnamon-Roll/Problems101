71b7560d3175754a9fd26756244bb7123271be90
Shinnamon-Roll <chimonkung0@gmail.com>
2024-09-24 15:03:34 +0700

    feat: currencyy conversion

--- 51-60/56currencyConversion.py
+++ 51-60/56currencyConversion.py
@@ -1,7 +1,12 @@
 def convertCurrency(amount: float, fromCurrency: str, toCurrency: str) -> float:
-    myD = {'eur': 0.85, 'gbp': 0.75, 'jpy': 110.0, 'thb': 32.0}
+    myD = {'usd':1, 'eur': 0.85, 'gbp': 0.75, 'jpy': 110.0, 'thb': 32.0}
 
-
+    for currency, money in myD.items():
+        if fromCurrency.lower() == currency:
+            amount /= money
+            for i, j in myD.items():
+                if toCurrency.lower() == i:
+                    return f"{amount * j:.2f}"
 
 def main():
     print("Supported Currencies\nUSD (United States Dollar)\nEUR (Euro)\nGBP (British Pound Sterling)\nJPY (Japanese Yen)\nTHB (Thai Baht)")
