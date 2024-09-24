cf946ee52e3669e769901c840817f64f064775bd
Shinnamon-Roll <chimonkung0@gmail.com>
2024-09-24 14:03:06 +0700

    feat: convert temp

--- 51-60/54convertTemperature.py
+++ 51-60/54convertTemperature.py
@@ -1,10 +1,21 @@
-def fahrenheit_to_celsius(fahrenheit: float) -> float:
-    pass
+def fahrenheitToCelsius(fahrenheit: float) -> float:
+    return (fahrenheit - 32) * (5 / 9)
 
-def celsius_to_fahrenheit(celsius: float) -> float:
-    pass
+def celsiusToFahrenheit(celsius: float) -> float:
+    return (celsius * (9/ 5)) + 32
 
 def main():
- 
+    choice = int(input("1.Fahrenheit To Celsius\n2.Celsius To Fahrenheit\nWhat do you want to do : "))
+    if choice == 1:
+        temperature = float(input("Enter fahrengeit to convert: "))
+        result = fahrenheitToCelsius(temperature)
+        print(result)
 
+    elif choice == 2:
+        temperature = float(input("Enter celsius to convert: "))
+        result = celsiusToFahrenheit(temperature)
+        print(result)
+
+    else:
+        print("Unknown choice")
 main()
