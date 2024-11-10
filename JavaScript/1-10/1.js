7548222410603bd64069af7a74aee33864bc389a
Shinnamon-Roll <chimonkung0@gmail.com>
2024-11-10 17:32:55 +0700

    release : 1.js

--- JavaScript/1-10/1.js
+++ JavaScript/1-10/1.js
@@ -1,1 +1,29 @@
-console.log("Hello world!")
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function findMultiplesOfThree(startingNumber, endNumber) {
+    let resultList = [];
+
+    for (let result = startingNumber; result <= endNumber; result += 1) {
+        if (result % 3 === 0 ) {
+            resultList.push(result);
+        }
+    }
+
+    console.log(`The results are : ${resultList}`)
+}
+
+function main() {
+    rl.question("Enter the starting number: ", (startNumber) => {
+        rl.question("Enter the ending number: ", (endNumber) => {
+            findMultiplesOfThree(parseInt(startNumber), parseInt(endNumber));
+            rl.close();
+        });
+    });
+}
+
+main();
