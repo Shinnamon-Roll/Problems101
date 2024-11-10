5944e7dc9fded9b41274fbfb8b8421994ce9ac90
Shinnamon-Roll <chimonkung0@gmail.com>
2024-11-10 19:37:31 +0700

    feat : 3.js

--- JavaScript/1-10/3.js
+++ JavaScript/1-10/3.js
@@ -3,7 +3,7 @@ const readline = require("readline");
 let rl = readline.createInterface({
     input : process.stdin,
     output : process.stdout
-});
+})
 
 function findNonMultiple(start, end) {
     resultList = [];
